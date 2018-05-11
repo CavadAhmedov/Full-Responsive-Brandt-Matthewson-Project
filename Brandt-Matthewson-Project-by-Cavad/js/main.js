588161314fa751e4a715791d9d8f0342e0566fc1
CavadAhmedov <36768138+CavadAhmedov@users.noreply.github.com>
2018-05-11 16:21:54 +0400

    Add files via upload

---
+++ Brandt-Matthewson-Project-by-Cavad/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+
+$(".fa-bars").click(function(){
+	$(".nav").toggleClass("hidden");
+})
+
+$(".fa-times-circle").click(function(){
+	$(".nav").toggleClass("hidden");
+})
+
+
+
+
+
+
+
+
+
+
+
+});
